c981e86a30fdc39b05baf2ae4e17dedb8650173f
piwpiwahh <bosfreecredit@gmail.com>
2024-08-12 20:28:48 +0800

    Update script.js

--- script.js
+++ script.js
@@ -7,7 +7,7 @@ document.addEventListener("DOMContentLoaded", function() {
       { url: "https://bila2cuci.asia/RF228839A71", imgSrc: "images/Screenshot 2024-08-12 065819.png", text: "bila2cuci" },
       { url: "https://vipklbet2.com/RF16355A919", imgSrc: "images/Screenshot 2024-08-12 103401.png", text: "vipklbet" },
       { url: "##", imgSrc: "https://lucky555a.com/media/0883f6ab8735699206e52.png", text: "##" },
-      { url: "##", imgSrc: "##", text: "##" },
+      { url: "##", imgSrc: "https://cashkingbet.net/media/cbf571caa5466ad94abdf.png", text: "##" },
       { url: "##", imgSrc: "##", text: "##" },
       
     ];
