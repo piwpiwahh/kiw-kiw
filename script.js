30a01981458eff2fa323224d2e96eb2c3438bc51
piwpiwahh <bosfreecredit@gmail.com>
2024-08-12 20:16:39 +0800

    Update script.js

--- script.js
+++ script.js
@@ -6,7 +6,7 @@ document.addEventListener("DOMContentLoaded", function() {
       { url: "https://pubg2like.com/RF322A2A3", imgSrc: "images/screenshot 2024-08-12 024242.png", text: "pubg2" },
       { url: "https://bila2cuci.asia/RF228839A71", imgSrc: "images/Screenshot 2024-08-12 065819.png", text: "bila2cuci" },
       { url: "https://vipklbet2.com/RF16355A919", imgSrc: "images/Screenshot 2024-08-12 103401.png", text: "vipklbet" },
-      { url: "##", imgSrc: "##", text: "##" },
+      { url: "##", imgSrc: "https://lucky555a.com/media/0883f6ab8735699206e52.png", text: "##" },
       { url: "##", imgSrc: "##", text: "##" },
       { url: "##", imgSrc: "##", text: "##" },
       
